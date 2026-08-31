ff404bae72b36c9e153c41233fed2106627b663e
Soldanellka <juliapluharova@gmail.com>
2026-08-31 20:20:47 +0200

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Reservio – Rezervačný systém",
+  description: "Univerzálny rezervačný systém pre každého"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="sk">
+      <body>{children}</body>
+    </html>
+  );
+}
