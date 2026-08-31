ce5607ffacc9652e46240463e8e72ee852aa408b
Soldanellka <juliapluharova@gmail.com>
2026-08-31 20:38:12 +0200

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import CalendarPreview from "../components/CalendarPreview";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <CalendarPreview />
+    </>
+  );
+}
