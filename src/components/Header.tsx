f84aebcfd788c79feca1376a11cd0fd426a0ff61
Soldanellka <juliapluharova@gmail.com>
2026-08-31 20:30:59 +0200

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <div class="logo">RESERVIO</div>
+      <nav class="nav">
+        <a href="#">Pre salóny</a>
+        <a href="#">Pre školy</a>
+        <a href="#">Prihlásiť sa</a>
+      </nav>
+    </header>
+  );
+}
